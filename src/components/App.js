337ba23d911a5533d1cd61e191ced3169516d64e
Ihor Poliakov <piv2003@gmail.com>
2023-07-11 00:02:59 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from 'components/Feedback';
 import { Statistics } from 'components/Statistics';
 import { Section } from 'components/Section';
