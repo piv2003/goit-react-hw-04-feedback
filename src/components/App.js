c81509901c24d2adf313598b34be4834e1c5f89b
Ihor Poliakov <piv2003@gmail.com>
2023-07-11 23:48:52 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,10 @@ export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
   const options = ['Good', 'Neutral', 'Bad'];
   const sumFeedback = good + neutral + bad;
 
-function onLeaveFeedback(evt) {
+  function onLeaveFeedback(evt) {
     switch (evt.target.textContent) {
       case 'Good':
         setGood(prev => prev + 1);
@@ -22,38 +21,28 @@ function onLeaveFeedback(evt) {
       case 'Bad':
         setBad(prev => prev + 1);
         break;
-      default:console.log('Something wrong');
+      default:
+        console.log('Something wrong');
         return;
     }
   }
 
-function countPositiveFeedbackPercentage() {
+  function countPositiveFeedbackPercentage() {
     if (sumFeedback === 0) return 0;
     const totalFeedback = (good / sumFeedback) * 100;
     return Math.round(totalFeedback);
   }
 
-
-
-
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = ['Good', 'Neutral', 'Bad'];
-    return (
-      <Section title="Please, leave feedback">
-        <FeedbackOptions
-          options={options}
-          onLeaveFeedback={this.onLeaveFeedback}
-        />
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-        />
-      </Section>
-    );
-  }
+  return (
+    <Section title="Please leave feedback">
+      <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={sumFeedback}
+        positivePercentage={countPositiveFeedbackPercentage()}
+      />
+    </Section>
+  );
 }
